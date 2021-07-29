27568c0161051a79628c56f47e02e9a808175d00
Bizblk <enemy_counter@mail.ru>
2021-07-29 21:00:57 +0300

    Initial Commit

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Виталий Оранский on 29.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
